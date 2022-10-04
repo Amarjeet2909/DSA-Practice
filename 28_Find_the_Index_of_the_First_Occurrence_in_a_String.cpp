35eed2e657a0264536756379c0545be3a302e0ef
Amarjeet Kumar Singh <ak4348225@gmail.com>
2022-10-04 19:42:20 +0530

    Create 28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp

---
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,24 @@
+// Solution 1
+class Solution {
+    int check_substring(string str1, string str2)
+{   int i,j;
+    int len1 = str1.length();
+    int len2 = str2.length();
+ 
+    for (i = 0; i <= len2 - len1; i++) {
+        for (j = 0; j < len1; j++)
+            if (str2[i + j] != str1[j])
+                break;
+ 
+        if (j == len1)
+            return i;
+    }
+ 
+    return -1;
+}
+public:
+    int strStr(string haystack, string needle) {
+        int position = check_substring(needle, haystack);
+        return position;
+    }
+};
