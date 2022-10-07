bd278e9cb2761cbb3f713600353f619baffd77fb
Amarjeet Kumar Singh <ak4348225@gmail.com>
2022-10-07 12:12:00 +0530

    commit

--- 88_Merge_Sorted_Array_Leetcode.cpp
+++ 88_Merge_Sorted_Array_Leetcode.cpp
@@ -9,3 +9,39 @@ public:
            sort(nums1.begin(),nums1.end());
     }
 };
+
+//solution 2
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+           int i=m-1;
+           int j=n-1;
+           int k= (m+n)-1;
+           while((i>=0) && (j>=0)){
+               if(nums1[i]==nums2[j]){
+                   nums1[k]=nums1[i];
+                   k--;
+                   i--;
+                   nums1[k]=nums2[j];
+                   k--;
+                   j--;
+               }
+               else if(nums1[i]<nums2[j]){
+                   nums1[k]=nums2[j];
+                   j--;
+                   k--;
+               }
+               else{
+                   nums1[k]=nums1[i];
+                   k--;
+                   i--;
+               }
+           }
+           while(i>=0){
+               nums1[k--]=nums1[i--];
+           }
+           while(j>=0){
+               nums1[k--]=nums2[j--];
+           }
+    }
+};
